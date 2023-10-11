16ab1391d75c95d56a399796dc9143501117ab56
ThereIsNoOne <szymon.s.lasota@gmail.com>
2023-10-11 21:08:38 +0200

    Fixed Ex2, now wrong indicator resets after each program iteration. Added documentation.

--- src/Ex2.java
+++ src/Ex2.java
@@ -1,15 +1,20 @@
 import java.util.Scanner;
 
 public class Ex2 {
+    /**
+     * Program checks if all chars in *compare* string are listed in *reference* string and
+     * prints out appropriate message. Enter 'stop' to exit the program.
+     * @param args command line arguments.
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Podaj łańcuch referencyjny: ");
         String reference = scanner.nextLine().toLowerCase();
 
-        int wrong = 0;
         while (true) {
-            System.out.print("Podaj łańcuch do porównania: ");
+            int wrong = 0;
+            System.out.print("Podaj łańcuch do porównania (stop to exit): ");
             String compare = scanner.nextLine().toLowerCase();
 
             if (compare.equals("stop")) {
