6d58fd01331dbf0a993b5f92d9e2a0a6b8209a66
ThereIsNoOne <szymon.s.lasota@gmail.com>
2023-10-12 10:13:28 +0200

    Added HEX code validation and documentation

--- src/Ex3.java
+++ src/Ex3.java
@@ -1,6 +1,13 @@
 import java.util.Scanner;
 
 public class Ex3 {
+
+
+    /**
+     * Program to transfer HEX code to decimal values.
+     * @param args command line arguments.
+     * @throws IllegalArgumentException when HEX code is invalid.
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -9,6 +16,14 @@ public class Ex3 {
 
         scanner.close();
 
+        if (k.length() != 6){
+            throw new IllegalArgumentException("HEX code have to have length of six.");
+        }
+
+        if (!isHexValid(k)) {
+            throw new IllegalArgumentException("HEX code should contain only numbers from 0 to 9 and" +
+                    "letters from a to f.");
+        }
         String s1 = k.substring(0, 2);
         String s2 = k.substring(2, 4);
         String s3 = k.substring(4, 6);
@@ -18,5 +33,21 @@ public class Ex3 {
         int b = Integer.parseInt(s3, 16);
         System.out.printf("(" + r + "," + g + "," + b + ")");
 
+    }
+
+    /**
+     * Checks if the HEX code is valid (contains only char a-f and numbers 0-9).
+     * @param value HEX code value.
+     * @return true if is valid otherwise false.
+     */
+    private static boolean isHexValid(String value) {
+        String reference = "0123456789abcdef";
+
+        for (int i = 0; i < value.length(); i++) {
+            if (reference.indexOf(value.charAt(i)) == -1) {
+                return false;
+            }
+        }
+        return true;
     }
 }
