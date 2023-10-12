7d1319e126f232bb7f9b91fb480e2f3b2c2aa4e7
ThereIsNoOne <szymon.s.lasota@gmail.com>
2023-10-12 09:50:43 +0200

    Fixed Ex3, s3 is now used and r, g, b are in correct order when printing

--- src/Ex3.java
+++ src/Ex3.java
@@ -15,8 +15,8 @@ public class Ex3 {
 
         int r = Integer.parseInt(s1, 16);
         int g = Integer.parseInt(s2, 16);
-        int b = Integer.parseInt(s2, 16);
-        System.out.printf("(" + r + "," + b + "," + g + ")");
+        int b = Integer.parseInt(s3, 16);
+        System.out.printf("(" + r + "," + g + "," + b + ")");
 
     }
 }
