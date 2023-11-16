e91d39ed4ed170cf91b4ac3b38d309b922ddbcf0
ThereIsNoOne <szymon.s.lasota@gmail.com>
2023-11-16 09:40:23 +0100

    Finished exercise 2

---
+++ src/main/java/list5/ProductType.java
@@ -0,0 +1,8 @@
+package list5;
+
+public enum ProductType {
+    CHOCOLATE,
+    LOLLIPOP,
+    CHEWING_GUM,
+    UNKNOWN
+}
---
+++ src/main/java/list5/Warehouse.java
@@ -0,0 +1,79 @@
+package list5;
+
+import list3.abstractimplementation.ChewingGum;
+import list3.abstractimplementation.Chocolate;
+import list3.abstractimplementation.Lollipop;
+import list3.abstractimplementation.Product;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Class represents warehouse.
+ */
+public class Warehouse<T extends Product> {
+    private final Map<ProductType, List<T>> products = new HashMap<>() {{
+        for (ProductType type: ProductType.values()) {
+            put(type, new ArrayList<>());
+        }
+    }};
+
+    /**
+     * Initialize warehouse object.
+     * @param products initial map of products in the warehouse. Might be
+     *                 extended with {@code addProduct} method or products can be
+     *                 removed with {@code removeProduct} method.
+     */
+    public Warehouse(List<T> products) {
+        products.forEach(this::addProductToMap);
+    }
+
+    private void addProductToMap(T product) {
+        if (product instanceof Lollipop) {
+            products.get(ProductType.LOLLIPOP).add(product);
+        } else if (product instanceof Chocolate) {
+            products.get(ProductType.CHOCOLATE).add(product);
+        } else if (product instanceof ChewingGum) {
+            products.get(ProductType.CHOCOLATE).add(product);
+        } else {
+            products.get(ProductType.UNKNOWN).add(product);
+        }
+    }
+
+    /**
+     * Retrieves products with certain type..
+     * @param type type of product
+     * @return product at index.
+     */
+    public List<T> getListOfProducts(ProductType type) {
+        return products.get(type);
+    }
+
+    /**
+     * Adds new product to products list.
+     * @param product product to be added.
+     */
+    public void addProduct(T product) {
+        addProductToMap(product);
+    }
+
+    /**
+     * Removes product from products list.
+     * @param product product to be removed.
+     */
+    public void removeProduct(ProductType type, T product) {
+        products.get(type).remove(product);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        for (ProductType key : products.keySet()) {
+            products.get(key).forEach((value) -> builder.append(value.toString()).append("\n"));
+        }
+        return builder.toString();
+    }
+
+}
